3591d19ea977b48f709d59d1be392d3f34cae90f
Nandhitha MP <121859489+nandhithamp@users.noreply.github.com>
2023-02-18 21:39:14 +0530

    Update bitstring.c

--- bitstring.c
+++ bitstring.c
@@ -1,60 +1,71 @@
 #include<stdio.h>
 #include<string.h>
-void unni(char*,char*,char*);
-void inter(char*,char*,char*);
-void diff(char*,char*,char*);
+void intersection(char*,char*,char*);
+void unionn(char*,char*,char*);
+void difference(char*,char*,char*);
 void main()
 {
-char s1[20],s2[20],s3[20];
-printf("enter the first string\n");
-scanf("%s",s1);
-printf("enter the second string\n");
-scanf("%s",s2);
-unni(s1,s2,s3);
-printf("union is%s\n",s3);
-inter(s1,s2,s3);
-printf("intersection is%s\n",s3);
-diff(s1,s2,s3);
-printf("difference is%s\n",s3);
+    char s1[20],s2[20],s3[40];
+    int i,l;
+    printf("Enter the first string:");
+    scanf("%s",s1);
+    printf("Enter the second string:");
+    scanf("%s",s2);
+    intersection(s1,s2,s3);
+    printf("Intersection is %s\n",s3);
+    unionn(s1,s2,s3);
+    printf("Union is %s\n",s3);
+    difference(s1,s2,s3);
+    printf("difference is %s\n",s3);
+    
 }
-void unni(char*s1,char*s2,char*s3)
+void intersection(char*s1,char*s2,char*s3)
 {
-int i,l;
-l=strlen(s1);
-for(i=0;i<l;i++)
-{
-if(s1[i]=='0' && s2[i]=='0')
-s3[i]='0';
-else
-s3[i]='1';
+    int l,i;
+    l=strlen(s1);
+    for(i=0;i<l;i++)
+    {
+        if(s1[i]=='1' && s2[i]=='1')
+        {
+            s3[i]='1';
+        }
+        else
+        {
+         s3[i]='0';
+        }
+    }
 }
-s2[i]='\0';
-}
-void inter(char*s1,char*s2,char*s3)
+void unionn(char*s1,char*s2,char*s3)
 {
-int i,l;
-l=strlen(s1);
-for(i=0;i<l;i++)
-{
-if(s1[i]=='1' && s2[i]=='1')
-s3[i]='1';
-else
-s3[i]='0';
+    int i,l;
+    l=strlen(s1);
+    for(i=0;i<l;i++)
+    {
+        if(s1[i]=='0' && s2[i]=='0')
+        {
+            s3[i]='0';
+        }
+        else
+        {
+            s3[i]='1';
+        }
+    }
 }
-s2[i]='\0';
-}
-void diff(char*s1,char*s2,char*s3)
+void difference(char*s1,char*s2,char*s3)
 {
-int i,l;
-l=strlen(s1);
-for(i=0;i<l;i++)
-{
-if(s1[i]=='1' && s2[i]=='0')
-s3[i]='1';
-else
-s3[i]='0';
-}
-s2[i]='\0';
+    int i,l;
+    l=strlen(s1);
+    for(i=0;i<l;i++)
+    {
+        if(s1[i]=='1' && s2[i]=='0')
+        {
+            s3[i]='1';
+        }
+        else
+        {
+            s3[i]='0';
+        }
+    }
 }
 
 
